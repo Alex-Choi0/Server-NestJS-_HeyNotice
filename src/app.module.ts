04c3e48430614fd6b0c50ffd5087a5c151ab66f2
alex <choijeaho86@gmail.com>
2022-05-18 22:47:44 +0900

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+    type: 'mysql', // 데이터베이스 타입
+    host: process.env.DB_HOST, // 주소
+    port: +process.env.DB_PORT, // 데이터베이스 포스트
+    username: process.env.DB_USERNAME, // 데이터베이스 소유자 이름
+    password: process.env.DB_PASSWORD, // 데이터베이스 비밀번호
+    database: process.env.DB_DATABASE, // 데이터베이스 이름
+    autoLoadEntities: true, // 자동으로 entities를 DB에 배포
+    synchronize: true // 동기화
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // DocumentBuilder import
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  // Swagger Document를 설정
+  const options = new DocumentBuilder()
+    .setTitle('nest API')
+    .setVersion('1.0')
+    .addBearerAuth
+    // Authentication이 가능하게 해주는 기능
+    ()
+    .build();
+
+  // Swagger Document의 문서를 api(/api-docs)로 설정할수 있게 셋팅
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document); 
+
+  await app.listen(3000);
+}
+bootstrap();
